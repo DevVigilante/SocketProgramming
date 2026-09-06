9dd96f8b46d1a17834f1cfb596c837a8412cb472
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2026-09-06 17:09:25 +0000

    feat: add ANSI colored output for bind errors

--- src/ServerSocket.cpp
+++ src/ServerSocket.cpp
@@ -13,11 +13,11 @@ ServerSocket::ServerSocket(int domain, int socketType, int protocol, int port, u
 void ServerSocket::attachSocketToAddress() {
   int bindingResult = bind(socketFileDecriptor, (struct sockaddr *)&address, sizeof(address));
   if (bindingResult == -1) {
-    std::cerr << "Failed to bind socket to the port: " << port << std::endl;
+    std::cerr << "\033[31mFailed to bind socket to the port: " << port << std::endl;
     std::cerr << "Message: " << std::strerror(errno) << std::endl;
-    std::cerr << "error(" << errno << ")" << std::endl;
+    std::cerr << "error(" << errno << ")\033[0m" << std::endl;
 
-    std::string message = "Failed to binding socket to port";
+    std::string message = "Failed to binding socket to port\n";
     throw std::runtime_error(message);
   }
 }
