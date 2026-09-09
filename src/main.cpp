a4238e54f4cbb229cef6509fbc765aa72031355b
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2026-09-09 17:23:46 +0000

    refactor: define PORT constant for server configuration

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+#define PORT 8000
+
 int main() {
 
   try {
-    ServerSocket mServer(AF_INET, SOCK_STREAM, 0, 8000, INADDR_ANY);
+    ServerSocket mServer(AF_INET, SOCK_STREAM, 0, PORT, INADDR_ANY);
   } catch (const std::runtime_error &e) {
     std::cerr << "Server thrown runtime error:\n";
     std::cerr << e.what() << std::endl;
